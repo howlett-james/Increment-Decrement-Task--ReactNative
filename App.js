345a12c7505f63d46f82eb037147cce43a11378e
YOKESHWARA RAO <yokeshwararao@hotmail.com>
2021-07-01 15:20:32 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    countIndex: 0,
+    countArray: [1, 2, 3, 4, 5, 6, 7, 8],
+  };
+
+  incrementValue = () => {
+    if (this.state.countIndex < this.state.countArray.length) {
+      this.setState({ countIndex: this.state.countIndex + 1 });
+    }
+    console.log(this.state.countArray[this.state.countIndex]);
+  };
+  decrementValue = () => {
+    if (this.state.countIndex > 0) {
+      this.setState({ countIndex: this.state.countIndex - 1 });
+    }
+    console.log(this.state.countArray[this.state.countIndex]);
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={{ fontSize: 40 }}>
+          {this.state.countArray[this.state.countIndex]}
+        </Text>
+
+        <Button title='Increment' onPress={this.incrementValue} />
+
+        <View style={{ padding: 30 }} />
+
+        <Button title='Decrement' onPress={this.decrementValue} />
+      </View>
+    );
+  }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
